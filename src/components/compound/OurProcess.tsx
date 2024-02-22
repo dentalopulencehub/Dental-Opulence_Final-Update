8812d64bcaf7de1e324459d8c92e117e27ef6f20
damoski2 <akogunoyindamola42@gmail.com>
2024-02-22 17:00:57 +0100

    chore: commit

--- src/components/compound/OurProcess.tsx
+++ src/components/compound/OurProcess.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import { home_process } from "../../../constants";
+import { ProcessCard } from "../molecule";
 
 import arrow_right_light from "../../../assets/images/arrow-right.svg";
 
@@ -8,7 +9,22 @@ const OurProcess = () => {
   return (
     <div className="bg-white sm:pt-5 sm:px-5 px-2 pt-2">
       <div className="w-full lg:px-[100px] sm:px-[40px] px-[20px] py-[80px] bg-[#100E10]">
-        
+        <div className="flex flex-wrap xl:justify-between justify-evenly">
+          <div className="xs:w-[388px] w-full h-[228px] bg-[#383638] rounded-lg py-8 px-6 flex flex-col justify-between">
+            <p className="text-base font-Pangram-Bold text-[#AFAFAF] ">
+              Our Process
+            </p>
+            <h3 className="text-[36px] font-Pangram-Bold text-white">
+              Streamlined Dental Excellence
+            </h3>
+            <button>
+              <span className="text-xs font-Pangram-Regular">Our story</span>
+            </button>
+          </div>
+          {home_process.map((data, index) => (
+            <ProcessCard key={index} {...data} />
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/components/molecule/ProcessCard.tsx
+++ src/components/molecule/ProcessCard.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Image from 'next/image'
+import { HomeOurServicesType } from '../../../types'
 
-const ProcessCard = () => {
+const ProcessCard = ({ description, image, title }: HomeOurServicesType) => {
     return (
-        <div>
+        <div className='xs:w-[388px] w-full h-[228px] bg-transparent' >
             
         </div>
     )
