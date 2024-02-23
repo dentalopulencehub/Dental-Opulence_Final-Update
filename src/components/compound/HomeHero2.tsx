db524c64293ea40c50ec87d6ef0a33019e7efe6e
damoski2 <akogunoyindamola42@gmail.com>
2024-02-23 11:08:05 +0100

    chore: line scroll animation

--- src/components/compound/HomeHero2.tsx
+++ src/components/compound/HomeHero2.tsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useRef } from "react";
 import Image from "next/image";
 import home_hero2_ill from "../../../assets/images/home-hero2-ill.svg";
 import underline_vector from "../../../assets/images/underline-vector.svg";
+import { useScroll, motion, useTransform } from "framer-motion";
+import { useIsomorphicLayoutEffect } from "../../../hooks";
 
 const HomeHero2 = () => {
+  let value = `Dental Opulence is a new dental clinic that offers a combination of
+  general dentistry and cosmetic dentistry services. The clinic is
+  equipped with modern facilities and technology to provide
+  exceptional dental care. The team at Dental Opulence is dedicated to
+  ensuring a comprehensive and comfortable dental experience for
+  patients. The clinic's focus is on maintaining oral health and
+  enhancing smiles through cosmetic procedures.`;
+
+  const element = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: element,
+    offset: ["start 0.9", "start 0.15"],
+  });
+
+  useIsomorphicLayoutEffect(() => {
+    //scrollYProgress.on("change", (e) => console.log(e));
+  }, []);
+
+  const words = value.split(" ");
+
   return (
     <div className="bg-white sm:px-5 px-2 w-full">
       <div className="bg-[#100E10] w-full lg:px-[100px] sm:px-[40px] px-[20px] py-[80px] flex xl:flex-row flex-col justify-between gap-[70px] xl:gap-0">
@@ -19,14 +42,24 @@ const HomeHero2 = () => {
             <span className="text-white font-Pangram-Regular">About us</span>
             <Image src={underline_vector} alt="" />
           </p>
-          <h4 className="text-white font-Pangram-Regular mt-6 sm:text-[30px] text-[20px] sm:leading-[43px] leading-[32px] xl:text-left text-center">
-            Dental Opulence is a new dental clinic that offers a combination of
-            general dentistry and cosmetic dentistry services. The clinic is
-            equipped with modern facilities and technology to provide
-            exceptional dental care. The team at Dental Opulence is dedicated to
-            ensuring a comprehensive and comfortable dental experience for
-            patients. The clinic&apos;s focus is on maintaining oral health and
-            enhancing smiles through cosmetic procedures.
+          <h4
+            className="text-white font-Pangram-Regular mt-6 sm:text-[30px] text-[20px] sm:leading-[36px] leading-[28px] xl:text-left text-center flex flex-wrap"
+            ref={element}
+          >
+            {words.map((word, index) => {
+              const start = index / words.length;
+              const end = start + 1 / words.length;
+              console.log(start, end);
+              return (
+                <Word
+                  key={index}
+                  range={[start, end]}
+                  progress={scrollYProgress}
+                >
+                  {word}
+                </Word>
+              );
+            })}
           </h4>
         </div>
       </div>
@@ -35,3 +68,20 @@ const HomeHero2 = () => {
 };
 
 export default HomeHero2;
+
+const Word = ({
+  children,
+  range,
+  progress,
+}: {
+  children: any;
+  range: any;
+  progress: any;
+}) => {
+  const opacity = useTransform(progress, range, [0.2, 1]);
+  return (
+    <motion.span style={{ opacity }} className="mr-2 mt-1.5">
+      {children}
+    </motion.span>
+  );
+};
