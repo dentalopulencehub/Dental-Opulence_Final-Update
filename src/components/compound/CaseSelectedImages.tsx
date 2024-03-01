11689f34b0c8e3c98ab6bc12fbb52409f96600b4
damoski2 <akogunoyindamola42@gmail.com>
2024-03-01 12:38:36 +0100

    chore: fixes

--- src/components/compound/CaseSelectedImages.tsx
+++ src/components/compound/CaseSelectedImages.tsx
@@ -10,7 +10,7 @@ const CaseSelectedImages = () => {
       <div className="bg-[#100E10] pt-10 w-full h-full lg:px-[100px] sm:px-[40px] px-[20px] gap-[42px] relative">
         <div className="flex flex-wrap items-center lg:justify-center justify-start gap-10">
           {selectedCase?.images.map((image: any, index: number) => (
-            <Image src={image} alt="images" key={index} loading="lazy" />
+            <Image src={image} alt="images" key={index} loading="lazy" width={580} height={326} className="rounded-2xl" />
           ))}
         </div>
       </div>
