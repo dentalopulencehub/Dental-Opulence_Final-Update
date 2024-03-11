2a62b3baa16a31b5a3c501b198a052856b7e245d
damoski2 <akogunoyindamola42@gmail.com>
2024-03-11 01:44:19 +0100

    chore: fix faq Title size on mobile

--- src/components/molecule/FaqAccordion.tsx
+++ src/components/molecule/FaqAccordion.tsx
@@ -9,7 +9,7 @@ const FaqAccordion = ({ answer, question }: FaqType) => {
   return (
     <div className="py-[37px] px-5 accordion-group rounded-lg cursor-pointer bg-[#282828]">
       <h3 className="accordion-menu flex flex-row items-center justify-between">
-        <span className="text-white text-[18px] font-Inter flex items-center gap-2">
+        <span className="text-white sm:text-[18px] text-sm font-Inter flex items-center gap-2">
           {" "}
           <Image src={faq_bullet_icon} alt="" />
           <span className="text-2xl">{question}</span>
@@ -17,7 +17,7 @@ const FaqAccordion = ({ answer, question }: FaqType) => {
         <Image src={faq_close_icon} alt="Plus" className="faq-plus" />
       </h3>
       {/*  <div className="faq_divider bg-[#CFCFCF] h-[1px] w-full mt-8" /> */}
-      <p className="accordion-content h-0 overflow-hidden text-[#C2C0C0] font-light text-sm font-Inter">
+      <p className="accordion-content h-0 overflow-hidden text-[#C2C0C0] font-light sm:text-sm text-xs font-Inter">
         {answer}
       </p>
     </div>
