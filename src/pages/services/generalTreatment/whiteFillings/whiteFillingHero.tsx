b0bc853dee68a87e21bb41a519a109f4d24824ec
aryan7424 <aryan@branddy.net>
2024-09-14 02:17:52 +0530

    Service page bug fix

--- src/pages/services/generalTreatment/whiteFillings/whiteFillingHero.tsx
+++ src/pages/services/generalTreatment/whiteFillings/whiteFillingHero.tsx
@@ -11,46 +11,48 @@ const WhiteFillingHero = () => {
       <div className="bg-[#FFFFFF] rounded-t-[24px] pt-[100px] pb-[100px] h-full w-full items-center flex xl:flex-row flex-col justify-between xl:px-[100px] sm:px-[40px] px-[20px]">
         <div className="flex flex-col xl:items-start items-center w-full xl:w-[50%] py-8">
           <div className="flex flex-row items-center gap-4">
-            <h1 className="font-Pangram-Bold sm:text-[60px] w-full sm:leading-[64px] text-[44px] xl:text-left text-center text-[#161616]">
+          <h1 className="font-Pangram-Bold sm:text-[60px] w-full  text-[44px] text-left  text-[#161616]">
               White Fillings
             </h1>
           </div>
-          <p className="text-[18px] text-[#161616] mt-6 lg:text-left text-center w-[90%] leading-tight" style={{ lineHeight: '1.6' }}>
-            We offer white fillings as cosmetic and functional solutions for
+          <p
+            className="text-[18px] text-[#161616] mt-6 text-left w-full leading-tight"
+            style={{ lineHeight: "1.6" }}
+          >             We offer white fillings as cosmetic and functional solutions for
             tooth decay. These tooth-colored alternatives to metal fillings
             restore decayed teeth and replace old fillings for a natural
             appearance.{" "}
           </p>
 
-          <div className="mt-6 flex-col items-center text-center md:text-left"> {/* Changed item-center to items-center and added text-center */}
-          <div className="flex gap-2 items-center justify-center md:justify-start"> {/* Added justify-center */}
+          <div className="mt-6 flex-col w-full items-center text-center md:text-left">
+          <div className="flex gap-2 w-full items-center justify-start">
               <Image src={right_thick} alt="right_thick" />
               <p className="text-[16px] text-[#161616] lg:text-left text-center">
                 0% Finance Options
               </p>
             </div>
 
-            <div className="flex gap-2 items-center justify-center md:justify-start mt-3"> {/* Added justify-center */}
-              <Image src={right_thick} alt="right_thick" />
+            <div className="flex gap-2 w-full items-center justify-start">
+            <Image src={right_thick} alt="right_thick" />
               <p className="text-[16px] text-[#161616] lg:text-left text-center">
                 Evening and weekend appointments
               </p>
             </div>
           </div>
 
-          <div className="flex gap-6 xs:mt-[56px] mt-[32px] sm:mx-0 mx-auto flex-row ">
+          <div className="flex gap-4 w-full md:w-auto xs:mt-[56px] mt-[32px] sm:mx-0 mx-auto flex-col md:flex-row ">
             <Link
               href="https://book.do.co.uk/"
               target="_blank"
               title="Book now"
-              className="py-4 px-8 rounded-[34px] text-white font-Pangram-Medium bg-[#100E10]  text-xs mt-6 hover:text-[#100E10] hover:bg-transparent border border-transparent hover:border-[#100E10] duration-0 cursor-pointer"
+              className="py-4 px-8 text-center w-full md:w-auto rounded-[34px] text-white font-Pangram-Medium bg-[#100E10]  text-xs mt-6 hover:text-[#100E10] hover:bg-transparent border border-transparent hover:border-[#100E10] duration-0 cursor-pointer"
             >
               Book now
             </Link>
             <PrimaryLink
               href="/about"
               title="About us"
-              style="py-4 px-8 rounded-[34px] font-Pangram-Medium text-xs mt-6 border border-[#100E10] bg-transparent text-[#100E10] hover:text-[#fff] hover:bg-[#100E10] hover:border-[#100E10] duration-0 cursor-pointer"
+              style="py-4 px-8 w-full md:w-auto text-center rounded-[34px] font-Pangram-Medium text-xs mt- md:mt-6 border border-[#100E10] bg-transparent text-[#100E10] hover:text-[#fff] hover:bg-[#100E10] hover:border-[#100E10] duration-0 cursor-pointer"
             />
           </div>
         </div>
